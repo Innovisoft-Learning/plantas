fba3b5e5b9d3dc1acc841f627d2f6b15b991f0cd
Davi-Alcantara <playsd65@gmail.com>
2023-10-25 20:44:07 -0300

    site

--- scripts/script-info.js
+++ scripts/script-info.js
@@ -1,46 +1,80 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtém referências aos elementos
-    const btnCaracteristicas = document.getElementById("btn-caracteristicas");
-    const btnBeneficios = document.getElementById("btn-beneficios");
-    const infoBox = document.getElementById("info");
-  
-    // Define os eventos de clique para os botões
-    btnCaracteristicas.addEventListener("click", () => showInfo("Características"));
-    btnBeneficios.addEventListener("click", () => showInfo("Benefícios"));
-  
-    // Função para mostrar informações e animar os botões
-    function showInfo(infoText) {
-      // Anima os botões diminuindo a largura
-      const buttons = document.querySelectorAll(".botao-opcao");
-      buttons.forEach(button => {
-        button.style.transition = "width 0.7s"; // Adicione uma transição de largura suave
-        button.style.width = "200px"; // Define a largura original para todos os botões
-      });
-  
-      // Define a largura do botão clicado
-      buttons.forEach(button => {
-        if (button.textContent === infoText) {
-          button.style.width = "200px"; // Largura aumentada apenas para o botão clicado
-          setTimeout(() => {
-            button.classList.add("ativo");
-          }, 300); // Adicione a classe 'ativo' após a animação
-        }
-      });
-      infoBox.style.display = 'flex';
-      // Mostra a parte de informações com uma transição suave
-      infoBox.style.transition = "opacity 0.3s"; // Adicione uma transição de opacidade suave
-      infoBox.style.opacity = 0;
-  
+let botaoEscolha = "None";
+
+function carac(){
+  botaoEscolha = "Caracteristícas"
+  console.log("escolheu caracteristicas")
+  showInfo(botaoEscolha, planta, variavelControle); // Isso carregaria as características da planta p1 da 
+
+}
+
+function benef(){
+  botaoEscolha = "Benefícios"
+  console.log("escolheu beneficios")
+  showInfo(botaoEscolha, planta, variavelControle); // Isso carregaria as características da planta p1 da 
+
+}
+
+// Variáveis globais para armazenar as referências aos elementos
+const btnCaracteristicas = document.getElementById("btn-caracteristicas");
+const btnBeneficios = document.getElementById("btn-beneficios");
+const infoBox = document.getElementById("info");
+
+// Função para carregar e exibir informações com base na região, planta selecionada e botão clicado
+function showInfo(infoText, plantaSelecionada, regiaoSelecionada) {
+  // Anima os botões diminuindo a largura
+  const buttons = document.querySelectorAll(".botao-opcao");
+  buttons.forEach(button => {
+    button.style.transition = "width 0.7s";
+    button.style.width = "200px";
+  });
+
+  // Define a largura do botão clicado
+  buttons.forEach(button => {
+    if (button.textContent === infoText) {
+      button.style.width = "200px";
       setTimeout(() => {
-        // Define o texto da caixa de informações
-        infoBox.innerHTML = `<p>${infoText}</p>`;
-  
-        // Remove a classe 'ativo' de todos os botões
-        btnCaracteristicas.classList.remove("ativo");
-        btnBeneficios.classList.remove("ativo");
-  
-        // Mostra a parte de informações
-        infoBox.style.opacity = 1;
-      }, 300); // Aguarde a animação dos botões antes de mostrar a informação
+        button.classList.add("ativo");
+      }, 300);
     }
+  });
+
+  infoBox.style.display = 'flex';
+  infoBox.style.transition = "opacity 0.3s";
+  infoBox.style.opacity = 0;
+
+  setTimeout(() => {
+    // Carregar informações com base na região atual, planta selecionada e botão clicado
+    fetch(`infos-json/${regiaoSelecionada}.json`)
+      .then(response => response.json())
+      .then(data => {
+        if (infoText == "Caracteristícas"){
+        const infoContent = data[plantaSelecionada].caracs;
+        infoBox.innerHTML = `<p>${infoContent}</p>`;
+        }
+        else if (infoText == "Benefícios"){
+          const infoContent = data[plantaSelecionada].benefs;
+        infoBox.innerHTML = `<p>${infoContent}</p>`;
+        }
+      })
+      .catch(error => console.error("Erro ao carregar conteúdo: " + error));
+
+    // Remove a classe 'ativo' de todos os botões
+    btnCaracteristicas.classList.remove("ativo");
+    btnBeneficios.classList.remove("ativo");
+
+    // Mostra a parte de informações
+    infoBox.style.opacity = 1;
+  }, 300);
+}
+
+// Exemplo de como você pode chamar a função showInfo com base na planta e região selecionadas
+
+let variavelControle = localStorage.getItem("variavelControle");
+let planta = localStorage.getItem("planta");
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Acesse a variável global diretamente, sem importar
+  console.log("Segunda pagina carregada. O valor das variaveis de controle são: ", planta, variavelControle);
+  showInfo(botaoEscolha, planta, variavelControle); // Isso carregaria as características da planta p1 da 
+  
 });
--- scripts/script-seletor.js
+++ scripts/script-seletor.js
@@ -1,9 +1,84 @@
+let planta = "None";
+console.log(planta)
+
+function planta1(){
+    console.log("p1 pressionada.")
+  planta = "p1";
+  localStorage.setItem("planta", planta);
+}
+function planta2(){
+  planta = "p2";
+  localStorage.setItem("planta", planta);
+
+}
+function planta3(){
+  planta = "p3";
+  localStorage.setItem("planta", planta);
+
+}
+function planta4(){
+  planta = "p4";
+  localStorage.setItem("planta", planta);
+
+}
+function planta5(){
+  planta = "p5";
+  localStorage.setItem("planta", planta);
+
+}
+function planta6(){
+  planta = "p6";
+  localStorage.setItem("planta", planta);
+
+}
+function planta7(){
+  planta = "p7";
+  localStorage.setItem("planta", planta);
+
+}
+function planta8(){
+  planta = "p8";
+  localStorage.setItem("planta", planta);
+
+}
+function planta9(){
+  planta = "p9";
+  localStorage.setItem("planta", planta);
+
+}
+function planta10(){
+  planta = "p10";
+  localStorage.setItem("planta", planta);
+
+}
+
+
+window.planta1 = planta1;
+window.planta2 = planta2;
+window.planta3 = planta3;
+window.planta4 = planta4;
+window.planta5 = planta5;
+window.planta6 = planta6;
+window.planta7 = planta7;
+window.planta8 = planta8;
+window.planta9 = planta9;
+window.planta10 = planta10;
+
 // Função para carregar e exibir o conteúdo de um arquivo JSON
 function carregarConteudo(arquivo) {
     fetch(arquivo)
         .then(response => response.json())
         .then(data => {
             document.getElementById("p1").textContent = data.p1;
+            document.getElementById("p2").textContent = data.p2;
+            document.getElementById("p3").textContent = data.p3;
+            document.getElementById("p4").textContent = data.p4;
+            document.getElementById("p5").textContent = data.p5;
+            document.getElementById("p6").textContent = data.p6;
+            document.getElementById("p7").textContent = data.p7;
+            document.getElementById("p8").textContent = data.p8;
+            document.getElementById("p9").textContent = data.p9;
+            document.getElementById("p10").textContent = data.p10;
             document.getElementById("regiao").textContent = data.regiao;
         })
         .catch(error => console.error("Erro ao carregar conteúdo: " + error));
